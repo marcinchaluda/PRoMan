cc5c8191fe7ef8d5bbc61bd0196f95f0abd3d74a
Robert Matląg <robert.matlag@gmail.com>
2020-07-14 18:47:57 +0200

    Append modal to body in HTML

--- static/js/modals.js
+++ static/js/modals.js
@@ -45,9 +45,13 @@ const body = document.querySelector('body');
 
 const newBoardModal = createModal('new-board-modal', 'Create new board');
 const newBoardButton = document.getElementById('new-board-button');
+body.appendChild(newBoardModal);
+
+const testSpan = document.querySelector('#new-board-modal > .modal-content > .modal-header > .close');
+const test = document.querySelector('.modal-body');
+const testText = document.createTextNode('moj tekst')
+test.appendChild(testText);
 
 newBoardButton.onclick = function(){
     newBoardModal.style.display = "block";
 }
-
-body.appendChild(newBoardModal);
