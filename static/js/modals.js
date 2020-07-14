78617e1c00a1be48201b7056290ac4743fef4348
Robert Matląg <robert.matlag@gmail.com>
2020-07-14 16:54:39 +0200

    Add function for creating modals

---
+++ static/js/modals.js
@@ -0,0 +1,53 @@
+function createModal(id, headerText) {
+    const modal = createElementWithClasses('div', ['modal']);
+    const modalContent = createElementWithClasses('div', ['modal-content']);
+    const modalHeader = createElementWithClasses('div', ['modal-header']);
+    const modalBody = createElementWithClasses('div', ['modal-body']);
+    const modalFooter = createElementWithClasses('div', ['modal-footer']);
+
+    const closeXButton = createElementWithClasses('span', ['close']);
+    const textXButton = document.createTextNode('x');
+    closeXButton.appendChild(textXButton);
+
+    closeXButton.onclick = function(){
+        modal.style.display = "none";
+    }
+
+    const headerDescription = createElementWithClasses('h2', []);
+    const textToAdd = document.createTextNode(headerText);
+
+    headerDescription.appendChild(textToAdd);
+
+    modalHeader.appendChild(closeXButton);
+    modalHeader.appendChild(headerDescription);
+
+    modalContent.appendChild(modalHeader);
+    modalContent.appendChild(modalBody);
+    modalContent.appendChild(modalFooter);
+
+    modal.appendChild(modalContent);
+
+    modal.setAttribute('id', id);
+
+    return modal
+}
+
+function createElementWithClasses(typeOfElement, listOfClasses) {
+    const element = document.createElement(typeOfElement);
+    for (let classOnList of listOfClasses){
+        element.classList.add(classOnList);
+    }
+
+    return element
+}
+
+const body = document.querySelector('body');
+
+const newBoardModal = createModal('new-board-modal', 'Create new board');
+const newBoardButton = document.getElementById('new-board-button');
+
+newBoardButton.onclick = function(){
+    newBoardModal.style.display = "block";
+}
+
+body.appendChild(newBoardModal);
