aeb3e367d08c2af64f86dbf7876239670eb2a25e
marcinchaluda <chaluda@interia.pl>
2020-07-14 14:04:59 +0200

    add default columns to table

--- static/js/dom.js
+++ static/js/dom.js
@@ -1,6 +1,6 @@
 // It uses data_handler.js to visualize elements
 import { dataHandler } from "./data_handler.js";
-const displayTablesURL = '/get-boards';
+const defaultColumns = ['New', 'In Progress', 'Testing', 'Done'];
 
 export let dom = {
     init: function () {
@@ -21,7 +21,6 @@ export let dom = {
         // retrieves boards and makes showBoards called
         dataHandler.getBoards(function(boards){
             dom.showBoards(boards);
-            console.log(boards)
         });
     },
     showBoards: function (boards) {
@@ -37,6 +36,8 @@ export let dom = {
 
         let boardsContainer = document.querySelector('#boards');
         boardsContainer.insertAdjacentHTML("beforeend", outerHtml);
+        const showFirstTableDetails = document.querySelector('.box-container div:first-child');
+        showFirstTableDetails.classList.add('.show');
     },
     loadCards: function (boardId) {
         // retrieves cards and makes showCards called
@@ -44,6 +45,11 @@ export let dom = {
     showCards: function (cards) {
         // shows the cards of a board
         // it adds necessary event listeners also
+        // let cardsContainer = document.querySelector('.cards-container');
+        // console.log(cardsContainer)
+        // pipe extension -> pipes value into a function
+        // cards |> generateBoardDetails |> cardsContainer.appendChild;
+        // cardsContainer.appendChild(generateBoardDetails(cards))
     },
     // here comes more features
 };
@@ -51,16 +57,35 @@ export let dom = {
 function generateBoards(boards) {
     let boardList = '';
 
-    for(let board of boards){
+    for(let [index, board] of boards.entries()){
         boardList += `
             <li class="flex-row-start">
-                <h3>${board.title}</h3>
-                <a href="#" type="button">
-                    <i class="fas fa-plus-circle"></i>New card
-                </a>
-                <div class="cards-container"></div>
+                <div class="title flex-row-start">
+                    <h3>${board.title}</h3>
+                    <a href="#" type="button">
+                        <i class="fas fa-plus-circle"></i>New card
+                    </a>
+                </div>
+                <div class="board-details flex-row-end">
+                    <i class="fas fa-ellipsis-h"></i>
+                </div>
             </li>
+            <div class="cards-container flex-row-start ${index == 0 ? 'show' : ''}"}>${generateBoardDetails()}</div>
         `;
     }
     return boardList;
+}
+
+function generateBoardDetails() {
+    let cardList = '';
+
+    for (let index in defaultColumns) {
+        cardList += `
+            <div class='cell'>
+                <h3>${defaultColumns[index]} ${index}</h3>
+                <div class="tasks flex-column">asdas</div>
+            </div>
+        `;
+    }
+    return cardList;
 }
