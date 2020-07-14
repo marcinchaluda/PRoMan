aabb71c7a116e91b76019f0d09102a78dfef5021
Robert Matląg <robert.matlag@gmail.com>
2020-07-15 00:03:01 +0200

    Refactor code: extract a few smaller function from fillNewBoardModal() to make code more readable

--- static/js/modals.js
+++ static/js/modals.js
@@ -1,8 +1,6 @@
 import {dataHandler} from "./data_handler.js";
 
-const body = document.querySelector('body');
-
-
+// function create basic template for modal
 function createModal(id, headerText) {
     const modal = createElementWithClasses('div', ['modal']);
     const modalContent = createElementWithClasses('div', ['modal-content']);
@@ -37,6 +35,7 @@ function createModal(id, headerText) {
     return modal
 }
 
+
 function createElementWithClasses(typeOfElement, listOfClasses) {
     const element = document.createElement(typeOfElement);
     for (let classOnList of listOfClasses) {
@@ -46,51 +45,70 @@ function createElementWithClasses(typeOfElement, listOfClasses) {
     return element
 }
 
-const newBoardModal = createModal('new-board-modal', 'Create new board');
-body.appendChild(newBoardModal);
-
-//----------------------------------------------------------
-
-function fillNewBoardModalFooter() {
+// Inject data to basic modal to create modal for adding new boards
+function fillNewBoardModal() {
     const modalContent = document.querySelector('#new-board-modal > .modal-content');
     const modalBody = document.querySelector('#new-board-modal > .modal-content > .modal-body');
     const modalFooter = document.querySelector('#new-board-modal > .modal-content > .modal-footer');
 
-    const myInput = document.createElement('input');
-    myInput.setAttribute('name', 'my_input');
-    myInput.setAttribute('type', 'text');
-    myInput.setAttribute('id', 'board-title');
+    const boardTitleInput = createNewTextInput('board-title')
+    modalBody.appendChild(boardTitleInput);
 
-    modalBody.appendChild(myInput);
-
-    const saveButton = createElementWithClasses('button', []);
-    const myForm = document.createElement('form');
-    saveButton.setAttribute('type', 'submit');
-    const textSaveButton = document.createTextNode('Save');
-    saveButton.appendChild(textSaveButton);
-
+    const saveButton = createSaveButton();
     modalFooter.appendChild(saveButton)
 
-    myForm.appendChild(modalBody);
-    myForm.appendChild(modalFooter);
+    const newBoardForm = createNewBoardForm(modalBody, modalFooter);
 
-    myForm.addEventListener('submit', function (evant) {
-        evant.preventDefault();
-        const modalNewBoard = document.querySelector('#new-board-modal');
-        const inputValue = document.getElementById('board-title').value
-        modalNewBoard.style.display = "none"
-        dataHandler.createNewBoard(inputValue, function (response) {
-            console.log(response);
-        });
-    })
-
-    modalContent.appendChild(myForm);
-
+    modalContent.appendChild(newBoardForm);
 }
 
-//----------------------------------------------------------
-fillNewBoardModalFooter(newBoardModal);
+function createNewTextInput(id) {
+    const newInput = document.createElement('input');
+    newInput.setAttribute('type', 'text');
+    newInput.setAttribute('id', id);
 
+    return newInput
+}
+
+function createSaveButton() {
+    const newButton = createElementWithClasses('button', []);
+    newButton.setAttribute('type', 'submit');
+
+    const textSaveButton = document.createTextNode('Save');
+    newButton.appendChild(textSaveButton);
+
+    return newButton
+}
+
+function createNewBoardForm(modalBody, modalFooter) {
+    const newForm = document.createElement('form');
+    newForm.appendChild(modalBody);
+    newForm.appendChild(modalFooter);
+    newForm.addEventListener('submit', function (evant) {
+        evant.preventDefault();
+        sendNewTitleToServer();
+    })
+
+    return newForm
+}
+
+function sendNewTitleToServer() {
+    const modalNewBoard = document.querySelector('#new-board-modal');
+    modalNewBoard.style.display = "none"
+
+    const inputValue = document.getElementById('board-title').value
+    dataHandler.createNewBoard(inputValue, function (response) {
+        //TODO zapisuje dane do DB, co robić z jakimś responsem po stronie frontu
+    });
+}
+
+// Add new basic modal to page for new board creation and fill with content
+const body = document.querySelector('body');
+const newBoardModal = createModal('new-board-modal', 'Create new board');
+body.appendChild(newBoardModal);
+fillNewBoardModal(newBoardModal);
+
+// Call modal on click New Board button
 const newBoardButton = document.getElementById('new-board-button');
 newBoardButton.onclick = function () {
     newBoardModal.style.display = "block";
