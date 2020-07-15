74174b961315439eca489b249a6e7f291c0aa96c
marcinchaluda <chaluda@interia.pl>
2020-07-15 09:20:36 +0200

    add handle detail button function

--- static/js/container_generator.js
+++ static/js/container_generator.js
@@ -1,9 +1,10 @@
 const defaultColumns = ['New', 'In Progress', 'Testing', 'Done'];
+const captureInnerTextContainer = 16;
 
 export function generateBoards(boards) {
     let boardList = '';
 
-    for(let [index, board] of boards.entries()){
+    for(let board of boards){
         boardList += `
             <li class="flex-row-start">
                 <div class="title flex-row-start">
@@ -16,7 +17,7 @@ export function generateBoards(boards) {
                     <i class="fas fa-ellipsis-h"></i>
                 </div>
             </li>
-            <div class="cards-container flex-row-start ${index == 0 ? 'show' : ''}"}>${generateBoardDetails()}</div>
+            <div class="cards-container flex-row-start hide-details"}>${generateBoardDetails()}</div>
         `;
     }
     return boardList;
@@ -28,10 +29,26 @@ export function generateBoardDetails() {
     for (let index in defaultColumns) {
         cardList += `
             <div class='cell'>
-                <h3>${defaultColumns[index]} ${index}</h3>
+                <h3>${defaultColumns[index]}</h3>
                 <div class="tasks flex-column">asdas</div>
             </div>
         `;
     }
     return cardList;
+}
+
+export function handleDetailButton() {
+    const detailBtn = document.querySelectorAll('.fas.fa-ellipsis-h');
+        console.log(detailBtn)
+        detailBtn.forEach(button => {
+            button.addEventListener('click', function () {
+                const cardsContainer = button.parentElement.parentElement.nextElementSibling;
+                cardsContainer.classList.toggle('show');
+                if (cardsContainer.style.maxHeight){
+                  cardsContainer.style.maxHeight = null;
+                } else {
+                  cardsContainer.style.maxHeight = cardsContainer.scrollHeight + captureInnerTextContainer + "px";
+                }
+            });
+        });
 }
