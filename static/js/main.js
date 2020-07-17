9b8866a3b65667d55107e0dc0a93243328571890
Robert Matląg <robert.matlag@gmail.com>
2020-07-17 10:07:24 +0200

    Refactor main.js: remove comments

--- static/js/main.js
+++ static/js/main.js
@@ -2,11 +2,8 @@ import { dom } from "./dom.js";
 import { modals } from "./modals.js";
 import { dragAndDropHandler} from "./drag_and_drop_handler.js";
 
-// This function is to initialize the application
 function init() {
-    // init data
     dom.init();
-    // loads the boards to the screen
     dom.loadBoards()
         .then(() => {
             modals.modalsInit();
