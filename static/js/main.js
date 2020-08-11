4abd16f5b1ba8fb22131ee8d28930d0d4d7f99ea
StolarzPrzemyslaw <stolarz.przemyslaw@gmail.com>
2020-08-11 17:07:28 +0200

    delete unused import from main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,6 @@
 import { dom } from "./dom.js";
 import { modalsInit } from "./modals.js";
 import { deleteButtonsInit } from "./board_handler.js";
-import { dragAndDropHandler} from "./drag_and_drop_handler.js";
 
 function init() {
     dom.init();
