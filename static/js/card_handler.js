640164661698d6f79f6fc8a7b2758bd252a17c73
Robert Matląg <robert.matlag@gmail.com>
2020-08-12 22:01:08 +0200

    Change name of class to more meaningful

--- static/js/card_handler.js
+++ static/js/card_handler.js
@@ -3,7 +3,7 @@ import {util} from "./util.js";
 
 export function createDeleteCardButton(task, cardId) {
     const deleteButton = util.addButton(['fas', 'fa-trash-alt']);
-    deleteButton.classList.add('card-trash');
+    deleteButton.classList.add('card-icon');
 
     deleteButton.onclick = function () {
         dataHandler.deleteCard(cardId, function () {
@@ -15,7 +15,7 @@ export function createDeleteCardButton(task, cardId) {
 
 export function createEditCardButton(cardId) {
     const editButton = util.addButton(['fas', 'fa-edit']);
-    editButton.classList.add('card-trash');
+    editButton.classList.add('card-icon');
 
     editButton.onclick = function () {
         const editCardModal = document.querySelector('#edit-card-modal');
